d10be9cc43b2132c8c4780e968550c0d14927978
eria-developer <eriddeveloper@gmail.com>
2024-07-28 22:21:41 +0300

    set up the files and routes

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import ProfileScreen from "./App/Screens/ProfileScreen";
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <ProfileScreen />
+    </View>
+  );
+};
+
+export default App;
---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Profile from "../screens/Profile";
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+      <Profile />
+    </View>
+  );
+};
+
+export default index;
